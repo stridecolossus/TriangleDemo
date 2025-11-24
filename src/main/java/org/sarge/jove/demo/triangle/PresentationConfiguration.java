c150bd95fdcd1e684084be42533748d4529695be
stridecolossus <chris.sarge@gmail.com>
2025-11-24 13:38:01 +0000

    Update PresentationConfiguration.java

--- src/main/java/org/sarge/jove/demo/triangle/PresentationConfiguration.java
+++ src/main/java/org/sarge/jove/demo/triangle/PresentationConfiguration.java
@@ -1,7 +1,8 @@
 package org.sarge.jove.demo.triangle;
 
-import org.sarge.jove.common.Colour;
+import org.sarge.jove.common.*;
 import org.sarge.jove.platform.desktop.Window;
+import org.sarge.jove.platform.vulkan.*;
 import org.sarge.jove.platform.vulkan.core.*;
 import org.sarge.jove.platform.vulkan.render.*;
 import org.springframework.context.annotation.*;
@@ -9,25 +10,27 @@ import org.springframework.context.annotation.*;
 @Configuration
 class PresentationConfiguration {
 	@Bean
-	public static VulkanSurface surface(Window window, Instance instance, VulkanCoreLibrary lib) {
+	static VulkanSurface surface(Window window, Instance instance, VulkanCoreLibrary lib) {
 		return new VulkanSurface(window, instance, lib);
 	}
 
 	@Bean
-	public static VulkanSurface.Properties properties(VulkanSurface surface, PhysicalDevice device) {
+	static VulkanSurface.Properties properties(VulkanSurface surface, PhysicalDevice device) {
 		return surface.properties(device);
 	}
 
 	@Bean
-	public static Swapchain swapchain(LogicalDevice dev, VulkanSurface.Properties properties) {
+	static Swapchain swapchain(LogicalDevice dev, VulkanSurface.Properties properties) {
 		return new Swapchain.Builder()
 				.count(2)
+				.extent(new Dimensions(1024, 768))
+				.format(new SurfaceFormatWrapper(VkFormat.B8G8R8A8_UNORM, VkColorSpaceKHR.SRGB_NONLINEAR_KHR))
 				.clear(new Colour(0.3f, 0.3f, 0.3f, 1))
 				.build(dev, properties);
 	}
 
 	@Bean
-	public static RenderPass pass(LogicalDevice dev, Swapchain swapchain) {
+	static RenderPass pass(LogicalDevice dev, Swapchain swapchain) {
 		final Subpass subpass = new Subpass.Builder()
 				.colour(Attachment.colour(swapchain.format()))
 				.build();
@@ -38,7 +41,7 @@ class PresentationConfiguration {
 	}
 
 	@Bean
-	public static Framebuffer framebuffer(Swapchain swapchain, RenderPass pass) {
+	static Framebuffer framebuffer(Swapchain swapchain, RenderPass pass) {
 		return Framebuffer.create(
 				pass,
 				swapchain.extents().rectangle(),
